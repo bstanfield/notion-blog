a2f603ef4ce61fc482102a9ea6375cb6d398b6d1
bstanfield <bnstnfld@gmail.com>
2020-03-19 21:14:10 -0700

    ben-ified

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,8 +7,6 @@ import styles from '../styles/header.module.css'
 const navItems: { label: string; page?: string; link?: string }[] = [
   { label: 'Home', page: '/' },
   { label: 'Blog', page: '/blog' },
-  { label: 'Contact', page: '/contact' },
-  { label: 'Source Code', link: 'https://github.com/ijjk/notion-blog' },
 ]
 
 const ogImageUrl = 'https://notion-blog.now.sh/og-image.png'
@@ -20,15 +18,12 @@ export default ({ titlePre = '' }) => {
     <header className={styles.header}>
       <Head>
         <title>{titlePre ? `${titlePre} |` : ''} My Notion Blog</title>
-        <meta
-          name="description"
-          content="An example Next.js site using Notion for the blog"
-        />
-        <meta name="og:title" content="My Notion Blog" />
-        <meta property="og:image" content={ogImageUrl} />
+        <meta name="description" content="Here be cobwebs..." />
+        <meta name="og:title" content="Ben Stanfield | BLOG" />
+        {/* <meta property="og:image" content={ogImageUrl} />
         <meta name="twitter:site" content="@_ijjk" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content={ogImageUrl} />
+        <meta name="twitter:image" content={ogImageUrl} /> */}
       </Head>
       <ul>
         {navItems.map(({ label, page, link }) => (
